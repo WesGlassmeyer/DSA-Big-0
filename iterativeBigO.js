66eb88d58a506de19af71b2152e516165e09d5bd
Wes <mwg1825@aol.com>
2021-03-24 21:17:05 -0600

    Create iterativeBigO.js

---
+++ iterativeBigO.js
@@ -0,0 +1,21 @@
+// 1
+// O(n) Linear time complexity
+//
+// 2
+// O(n) Linear time complexity
+//
+// 3
+// O(n) Linear time complexity
+//
+// 4
+// O(n) Linear time complexity
+//
+// 5
+// O(n) Linear time complexity
+//
+// 6
+// O(n) Linear time complexity
+//
+// 7
+// O(n) Linear time complexity
+//
